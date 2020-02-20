cdb878d5e1c870df76eebbb119eba700a453008e
Vadim Bukov <dantrobin@gmail.com>
2020-02-20 17:59:32 +0200

    almost done

--- classes/Herald.php
+++ classes/Herald.php
@@ -11,7 +11,7 @@ class Herald
      * @var string
      * Гласные
      */
-    public $vowels = ['а','е','и','о','у','ы','э','ю','я'];
+    public $vowels = ['а','е','ё','и','о','у','ы','э','ю','я'];
 
     /**
      * @var string
@@ -33,29 +33,114 @@ class Herald
     public function openSyllable()
     {
         // случайное кол-во символов в слоге от 1 до 4
-        $syllablesCount = mt_rand(1, 4);
+        $symbolsCount = mt_rand(1, 4);
         $openSyllable = '';
 
-        if ( $syllablesCount === 1 ) $openSyllable = $this->getSymbol($this->vowels);
+        if ( $symbolsCount === 1 ) $openSyllable = $this->getSymbol($this->vowels);
 
-        if ( $syllablesCount === 2 ) $openSyllable = $this->getSymbol($this->consonants) . $this->getSymbol($this->vowels);
+        if ( $symbolsCount === 2 ) $openSyllable = $this->getSymbol($this->consonants) . $this->getSymbol($this->vowels);
 
-        if ( $syllablesCount > 2 )
+        if ( $symbolsCount > 2 )
         {
             $firstSymbol = mt_rand(0, 1) ? $this->getSymbol($this->consonants) : $this->getSymbol($this->vowels);
-            $third = $syllablesCount === 3 ? '' : $this->getSymbol($this->consonants);
-            $openSyllable =  $firstSymbol . $this->getSymbol($this->consonants) . $third . $this->getSymbol($this->vowels);
+            $openSyllable =  $firstSymbol . $this->getSymbol($this->consonants) . $this->getSymbol($this->vowels);
+            if ( $symbolsCount === 4 ) $openSyllable .= 'й';
         }
-        
+
         return $openSyllable;
     }
+
 
     /**
      * Закрытый слог - оканчивается на согласный звук.
      */
     public function closedSyllable()
     {
+        $symbolsCount = mt_rand(2, 4);
+        $closedSyllable = '';
 
+        if ( $symbolsCount === 2 ) $closedSyllable = $this->getSymbol($this->vowels) . $this->getSymbol($this->consonants);
+        if ( $symbolsCount > 2 )
+        {
+            $closedSyllable = $this->getSymbol($this->consonants).$this->getSymbol($this->vowels).$this->getSymbol($this->consonants);
+            if ( $symbolsCount === 4 ) $closedSyllable .= 'ь';
+        }
+
+        return $closedSyllable;
+    }
+
+
+    /**
+     * @param bool $firstUpper
+     * генерирует слово случайной длинны
+     * @return string
+     */
+    public function word( $firstUpper = false )
+    {
+        $symbolsCount = mt_rand(1, 14);
+
+        $word = '';
+
+        while ( strlen($word) < $symbolsCount )
+        {
+            $syllable = mt_rand(0, 1) ? $this->openSyllable() : $this->closedSyllable();
+            $word .= $syllable;
+        }
+
+        if ( $firstUpper )
+        {
+            $characters = preg_split( '//u', $word, -1, PREG_SPLIT_NO_EMPTY );
+            $characters[0] = mb_strtoupper($characters[0]);
+            $word = implode($characters);
+        }
+
+        return $word;
+    }
+
+    /**
+     * Формируем предложение
+     * @return string
+     */
+    public function sentence()
+    {
+        $wordsCount = mt_rand(3, 10);
+        $sentence = [];
+
+        $firstWord = $this->word(1);
+        array_push($sentence, $firstWord);
+
+        while ( count($sentence) < $wordsCount )
+        {
+            array_push($sentence, $this->word());
+        }
+
+        return implode(' ', $sentence) . '.';
+    }
+
+    public function paragraph()
+    {
+        $sentencesCount = mt_rand(4, 20);
+        $paragraph = [];
+
+        while ( count($paragraph) < $sentencesCount )
+        {
+            array_push($paragraph, $this->sentence());
+        }
+
+        return "&nbsp;&nbsp;&nbsp;&nbsp;" . implode(' ', $paragraph);
+    }
+
+    public function text()
+    {
+        $paragraphsCount = mt_rand(3, 10);
+        $text = [];
+
+        while ( count($text) < $paragraphsCount )
+        {
+            array_push($text, $this->paragraph());
+        }
+
+        return implode("<br>", $text);
     }
 
 }
--- index.php
+++ index.php
@@ -1,7 +1,27 @@
 <?php
+function debug( $arr, $str = false , $die = 0)
+{
+    echo '<pre>';
+    if ( $str ) echo $str . ' = ';
+    print_r( $arr );
+    echo '</pre>';
+
+    if ( $die ) die;
+}
 
 require_once __DIR__ . "/classes/Herald.php";
 
 $speaker = new Herald();
 
 echo $speaker->openSyllable();
+echo "<br>";
+echo $speaker->closedSyllable();
+
+echo "<br>";
+echo $speaker->word(true);
+echo "<br>";
+echo $speaker->sentence();
+echo "<br>";
+echo $speaker->paragraph();
+
+echo "<h4>".$speaker->text()."</h4>";
